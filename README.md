9a2fd731d368f1ee6481c895d097d503c518ca9d
Claudiu Cristea <clau.cristea@gmail.com>
2023-07-05 23:20:10 +0300

    Initial code

---
+++ README.md
@@ -0,0 +1,2 @@
+Exposes a Drush command that shows a tree of modules having a given module as
+dependency. Useful to understand the dependency chain in a Drupal installation.
---
+++ src/Drush/Commands/WhyModuleDrushCommands.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Drush\Commands;
+
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Extension\Dependency;
+use Drush\Attributes as CLI;
+use Drush\Boot\DrupalBootLevels;
+use Drush\Commands\DrushCommands;
+
+/**
+ * Drush commands to reveal module dependencies.
+ */
+class WhyModuleDrushCommands extends DrushCommands
+{
+    private const CIRCULAR_REFERENCE = 'circular_reference';
+    private array $dependencies = [];
+    private array $tree = [];
+    private array $relation = [];
+    private array $canvas = [];
+
+    #[CLI\Command(name: 'pm:why-module', aliases: ['why'])]
+    #[CLI\Help(description: 'List the all modules that depend on a given module')]
+    #[CLI\Argument(name: 'module', description: 'The module to check dependencies for')]
+    #[CLI\Bootstrap(level: DrupalBootLevels::FULL)]
+    public function why(string $module): ?string
+    {
+        $list = \Drupal::service('extension.list.module')->getList();
+        if (!isset($list[$module])) {
+            throw new \InvalidArgumentException(dt('Invalid @module module', [
+                '@module' => $module,
+            ]));
+        }
+
+        $this->buildDependencies($list);
+
+        if (!isset($this->dependencies[$module])) {
+            $this->logger()->notice(dt('No other module depends on @module', [
+                '@module' => $module,
+            ]));
+            return NULL;
+        }
+
+        $this->canvas[] = $module;
+        $this->buildTree($module);
+
+        return implode("\n", $this->canvas);
+    }
+
+    /**
+     * @param string $dependency
+     * @param array $path
+     * @param string $indent
+     */
+    protected function buildTree(string $dependency, array $path = [], string $indent = ''): void
+    {
+        $path[] = $dependency;
+        $dependants = $this->dependencies[$dependency];
+        foreach (array_keys($dependants) as $delta => $module) {
+            $lastFromThisLevel = $delta + 1 < count($dependants);
+            $char = $lastFromThisLevel ? '┣' : '┗';
+            $stroke = $indent . " {$char}━" . $module;
+            if (!NestedArray::keyExists($this->tree, $path)) {
+                NestedArray::setValue($this->tree, $path, []);
+            }
+
+            $circularReference = isset($this->relation[$dependency]) && $this->relation[$dependency] === $module;
+            if ($circularReference) {
+                // This relation has been already defined on other path. We mark it as
+                // circular reference.
+                NestedArray::setValue($this->tree, [...$path, ...[$module]], self::CIRCULAR_REFERENCE);
+                $stroke .= ' [' . dt('circular reference') . ']';
+            }
+
+            // Draw a new line to the canvas.
+            $this->canvas[] = $stroke;
+
+            if ($circularReference) {
+                continue;
+            }
+
+            // Save this relation to avoid infinite circular references.
+            $this->relation[$dependency] = $module;
+            if (isset($this->dependencies[$module])) {
+                $char = $lastFromThisLevel ? '┃' : ' ';
+                $this->buildTree($module, $path, $indent . " $char ");
+            }
+            else {
+                NestedArray::setValue($this->tree, [...$path, ...[$module]], []);
+            }
+        }
+    }
+
+    /**
+     * @param array $list
+     */
+    protected function buildDependencies(array $list): void {
+        foreach ($list as $module => $data) {
+            foreach ($data->info['dependencies'] as $dependencyString) {
+                $dependency = Dependency::createFromString($dependencyString)->getName();
+                $this->dependencies[$dependency][$module] = $module;
+            }
+        }
+    }
+}
